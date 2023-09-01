5af7b4d8ab4e46a1f45304a0881c660dd0805f4e
awpadilha <awpadilha.dev@gmail.com>
2023-08-31 23:15:03 -0300

    first commit

---
+++ login_test.js
@@ -0,0 +1,15 @@
+Feature('login');
+
+Scenario('Login com sucesso',  ({ I }) => {
+
+I.amOnPage('http://automationpratice.com.br/')
+
+
+})
+
+Scenario('Login com falha',  ({ I }) => {
+
+I.amOnPage('http://automationpratice.com.br/')
+    
+    
+})
