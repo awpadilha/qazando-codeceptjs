bc38998136e4dd7c504337a330166d72d68d773d
awpadilha André Padilha <awpadilha.dev@gmail.com>
2023-09-02 17:07:55 -0300

    Implemetando preeenchimentos de dados

--- login_test.js
+++ login_test.js
@@ -3,13 +3,33 @@ Feature('login');
 Scenario('Login com sucesso',  ({ I }) => {
 
 I.amOnPage('http://automationpratice.com.br/')
+I.click('Login') 
+I.fillField('#user','padilha.a.w@gmail.com')
 
 
 })
 
-Scenario('Login com falha',  ({ I }) => {
+Scenario('Tentando logar digitando apenas o e-mail',  ({ I }) => {
 
 I.amOnPage('http://automationpratice.com.br/')
     
     
 })
+
+
+Scenario('Tentando logar sem digitar e-mail e senha',  ({ I }) => {
+
+I.amOnPage('http://automationpratice.com.br/')
+        
+        
+})
+    
+Scenario('Tentando logar digitando apenas a senha',  ({ I }) => {
+
+I.amOnPage('http://automationpratice.com.br/')
+        
+        
+})
+    
+
+
