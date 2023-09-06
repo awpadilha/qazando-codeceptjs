21b343ed0bd69fbc21c55133e89f38e5be1bddc5
awpadilha André Padilha <awpadilha.dev@gmail.com>
2023-09-05 22:32:05 -0300

    Implementando preenchimentos de dados

--- login_test.js
+++ login_test.js
@@ -5,29 +5,38 @@ Scenario('Login com sucesso',  ({ I }) => {
 I.amOnPage('http://automationpratice.com.br/')
 I.click('Login') 
 I.fillField('#user','padilha.a.w@gmail.com')
+I.fillField('#password','qazando00##')  
+I.click('#btnLogin')
+I.waitForText('Login realizado', 3)
 
-
-})
+}).tag('@sucesso')
 
 Scenario('Tentando logar digitando apenas o e-mail',  ({ I }) => {
 
 I.amOnPage('http://automationpratice.com.br/')
-    
+I.click('Login') 
+I.fillField('#user','padilha.a.w@gmail.com')
+I.click('#btnLogin')
+I.waitForText('Senha inválida', 3)  
     
 })
-
-
+ 
 Scenario('Tentando logar sem digitar e-mail e senha',  ({ I }) => {
 
 I.amOnPage('http://automationpratice.com.br/')
-        
+I.click('Login')
+I.click('#btnLogin')
+I.waitForText('Login realizado', 3)  
         
 })
     
 Scenario('Tentando logar digitando apenas a senha',  ({ I }) => {
 
 I.amOnPage('http://automationpratice.com.br/')
-        
+I.click('Login')
+I.fillField('#password','qazando00##')
+I.click('#btnLogin')
+I.waitForText('Login realizado', 3)       
         
 })
     
